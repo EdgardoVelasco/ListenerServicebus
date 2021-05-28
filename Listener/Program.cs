42317f1b31ddede4dd582812a6302b422bf99045
edgardo.nvf <edgardo.nvf@gmail.com>
2021-05-28 16:13:39 -0500

    Listener

---
+++ Listener/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using Listener.bus;
+using Azure.Messaging.ServiceBus;
+namespace Listener
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine(".--.Recibiendo mensajes desde Service bus-.-.");
+            var cliente = Busclient.Cliente;
+            ReceiveMessagesFromSubscriptionAsync(cliente, "tienda", "app").Wait();
+            Console.WriteLine();
+        }
+
+        static async Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body} ");
+
+            // complete the message. messages is deleted from the queue. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+        static async Task ReceiveMessagesFromSubscriptionAsync(ServiceBusClient client, string topicName, string subscriptionName)
+        {
+            
+                // create a processor that we can use to process the messages
+                ServiceBusProcessor processor = client.CreateProcessor(topicName, subscriptionName, new ServiceBusProcessorOptions());
+
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                await processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping the receiver...");
+                await processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            
+        }
+    }
+}
---
+++ Listener/bus/Busclient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Azure.Messaging.ServiceBus;
+
+
+namespace Listener.bus
+{
+    class Busclient
+    {
+        private static readonly string CONN_STRING = "Endpoint=sb://serviceenvf.servicebus.windows.net/;SharedAccessKeyName=Listener;SharedAccessKey=vn2h4wJPjTadnq4GLqhj34CEof4nSEJ0KLbC8V739Tg=";
+        
+
+        public static ServiceBusClient Cliente { get; private set; }
+
+        static Busclient() { InitBus(); }
+
+        private static void InitBus() {
+            if (Cliente==null) {
+                Cliente = new ServiceBusClient(CONN_STRING);
+            }
+        }
+
+
+    }
+}
